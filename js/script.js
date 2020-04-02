afbd817e4b960a7f54aed9a7af669288e9ac1d00
Damecek <stepanek.ada@seznam.cz>
2020-04-02 23:29:25 +0200

    movement

--- js/script.js
+++ js/script.js
@@ -34,6 +34,16 @@ let board = [
 let wall = new Image();
 wall.src =  '../src/wall.png'
 
+let player ={
+    x: 8,
+    y: 1
+};
+
+let hero = new Image();
+hero.src = '../src/down.png';
+
+let keys = [];
+
 function renderBoard() {
     for (let row = 0; row < board.length; row++) {
         for (let col = 0; col < board.length; col++){
@@ -42,4 +52,45 @@ function renderBoard() {
     }
 }
 
-window.addEventListener("load", renderBoard);
+function draw() {
+    ctx.clearRect(player.x * blockSize, player.y * blockSize, blockSize, blockSize);
+    renderBoard();
+    movement();
+    ctx.drawImage(hero, player.x * blockSize, player.y * blockSize, blockSize, blockSize);
+}
+
+function movement() {
+    if (keys[39]) {
+        // šipka doprava
+        hero.src = "../src/right.png";
+        player.x++;
+    }
+ 
+    if (keys[37]) {
+        // šipka doleva
+        hero.src = "../src/left.png";
+        player.x--;
+    }
+ 
+    if (keys[38]) {
+        // šipka nahoru
+        hero.src = "../src/up.png";
+        player.y--;
+    }
+ 
+    if (keys[40]) {
+        // šipka dolů
+        hero.src = "../src/down.png";
+        player.y++;
+    }
+}
+
+window.addEventListener('load', draw);
+window.addEventListener('keydown', function(e){
+    keys[e.keyCode] = true;
+    draw();
+});
+window.addEventListener('keyup', function(e){
+    keys[e.keyCode] = false;
+    draw();
+});
